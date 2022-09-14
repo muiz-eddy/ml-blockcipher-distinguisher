55699c2d6b7d0f373022ad2427d5a10a342b1bf6
Muiz-Eddy <109058675+Muiz-Eddy@users.noreply.github.com>
2022-09-14 12:59:01 +0800

    Add files via upload

--- MachineLearning Distinguisher/machine_learning/data_generation.py
+++ MachineLearning Distinguisher/machine_learning/data_generation.py
@@ -1,33 +1,40 @@
-import sys, os
+from speck import SpeckCipher
+import sys
+import os
 import numpy as np
-parent_dir = os.getcwd() 
+
+parent_dir = os.getcwd()
 sys.path.append(parent_dir)
 
-from cipher_implementation.speck import SpeckCipher
 
-def td_speck(data_sample = 2**5, diff_c = [79042080,100000]): #input differences taken from Machine Learning Attacks On SPECK baksi et.
-   X = np.empty((0,1),int)
-   Y = np.empty((0),int)
-   P = np.frombuffer(os.urandom(4*data_sample),dtype = np.uint32)
-   K = np.frombuffer(os.urandom(8*data_sample),dtype= np.uint64)
+# input differences taken from Machine Learning Attacks On SPECK baksi et.
+def td_speck(data_sample=2**2, diff_c=[79042080, 100000, 52030701, 8710609]):
+    X = np.empty((0,2), int)
+    P = np.frombuffer(os.urandom(4*data_sample), dtype=np.uint32)
+    K = np.frombuffer(os.urandom(8*data_sample), dtype=np.uint64)
+    Plaintext = P.tolist()
+    Keys = K.tolist()
+    for i in range(len(diff_c)):
+        P1 = P ^ diff_c[i]
+        P1 = P1.tolist()
+        for j in range(data_sample):
+            C = SpeckCipher(Keys[j], 64, 32, 'ECB').encrypt(Plaintext[i])
+            C1 = SpeckCipher(Keys[j], 64, 32, 'ECB').encrypt(P1[j])
+            X = np.append(X, np.array([[C ^ C1,i]]), axis=0)
+    X_train = X[:,0:1]
+    Y_train = X[:,1]
+    return X_train, Y_train
+    
 
-   Plaintext = P.tolist()
-   Keys = K.tolist()
-   
-   for i in range(len(diff_c)):
-    P1 = P ^ diff_c[i] #
-    P1 = P1.tolist() #
-    for j in range(data_sample):
-        C = SpeckCipher(Keys[j], 64, 32, 'ECB').encrypt(Plaintext[i])
-        C1 = SpeckCipher(Keys[j], 64, 32, 'ECB').encrypt(P1[j])
-        X = np.append(X,np.array([[C ^ C1]]), axis = 0)
-        Y = np.append(Y,np.array([i]), axis = 0)
-    return X,Y
-
-
-
-
-
-
+def test_data_speck(data_sample=2**2, diff_c=[79042080, 100000, 52030701, 8710609]):
+    X = np.empty((0,2), int)
+    P = np.frombuffer(os.urandom(4*data_sample), dtype=np.uint32)
+    K = np.frombuffer(os.urandom(8*data_sample), dtype=np.uint64)
+    Plaintext = P.tolist()
+    Keys = K.tolist()
+    for i in range(len(diff_c)):
+        P1 = P ^ diff_c[i]
+        P1 = P1.tolist()
+        
 
 
